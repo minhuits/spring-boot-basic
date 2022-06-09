bd5495b4c8a22d75936a0dd9ee97d375969a49c4
ksksks <a41417144@gmail.com>
2022-06-09 21:11:04 +0900

    비동기 async 연습

--- src/main/java/com/company/basic/BasicApplication.java
+++ src/main/java/com/company/basic/BasicApplication.java
@@ -2,10 +2,10 @@ package com.company.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@ServletComponentScan
+@EnableAsync
 public class BasicApplication {
 
     public static void main(String[] args) {
