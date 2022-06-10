3172b683c3d0cc31b4a0de1f17f640ec69712fe8
ksksks <a41417144@gmail.com>
2022-06-10 19:53:00 +0900

    Rest Template 연습 01

---
+++ src/main/java/com/company/basic/client/controller/ClientController.java
@@ -0,0 +1,23 @@
+package com.company.basic.client.controller;
+
+import com.company.basic.client.dto.UserResponse;
+import com.company.basic.client.service.RestTemplateService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/client")
+public class ClientController {
+
+    private final RestTemplateService restTemplateService;
+
+    public ClientController(RestTemplateService restTemplateService) {
+        this.restTemplateService = restTemplateService;
+    }
+
+    @GetMapping("/hello")
+    public UserResponse getHello() {
+        return restTemplateService.hello();
+    }
+}
